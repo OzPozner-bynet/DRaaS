af526f8eaef4fab84925569f6f35212650de9aa7
Golansho <xccxxxccx@gmail.com>
2023-03-13 16:53:04 +0200

    update redis

--- redis_manage.py
+++ redis_manage.py
@@ -14,8 +14,8 @@ update_req_url = snow_url+"/SetCommandStatus"
 
 def redis_set(KEY="",VALUE=""):
     redis_server.set(name=KEY, value=VALUE)
-    key_val = redis_server.get(KEY)
-    print(f"{KEY} : {key_val}")
+    # key_val = redis_server.get(KEY)
+    # print(f"{KEY} : {key_val}")
 
 def get_requests():
     commands = requests.post(get_cmds_url, headers={'Content-Type': 'application/json'}, auth=('admin','Danut24680')).json()
@@ -36,32 +36,50 @@ def send_status_update(ID, STATUS, OUTPUT):
 
 def redis_queue_push(TASKS):
     for TASK in TASKS:
-        if TASK["dr_status"] == "active":
+        if bool(re.search('(active|failed)', TASK["dr_status"])):
             redis_server.rpush(queue_name, str(TASK))
 
 def redis_queue_get():
     req = redis_server.lpop(queue_name)
     return req
 
-if __name__ == "__main__":
-#     redis_queue_push(json.dumps(
-# {
-#     "result":
-#        {
-#             "command_number": "DRA0001010",
-#             "record_id": "fc1001ab8791a550220a98a83cbb35cc",
-#             "command": "",
-#             "switch": "2aa1ebb587571d905db3db1cbbbb359d",
-#             "switch_status": "on",
-#             "switch_ip": "192.168.88.30",
-#             "interface_name": "Gi0/9",
-#             "port_mode": "trunk",
-#             "dr_status": "Send_to_switch",
-#             "vlans": "60,61,62"
-#         }
-# }
-# ).replace("\n", ""))
+def json_parser(to_parse):
+    first_setup = to_parse.replace('\\n', '\n').replace('\\r', '\r').replace("'!","")
+    clean = re.sub("(\'|\,)", "", first_setup)
+    if re.search(r'(^\[|\]$)', clean):
+        switch_braces = re.sub("\]$","}",re.sub("^\[","{",clean))
 
+    formated = ""
+
+    empty_lines = 0
+    for e_line in switch_braces.splitlines():
+        if len(re.findall(r'\w+', e_line)) == 1 or len(re.findall(r'\w+\-\w+\-?\w?', e_line)) == 1 or not e_line.strip():
+            empty_lines+=1
+    num_of_lines = len(switch_braces.splitlines()) - empty_lines
+    line_num = 1
+    for line in switch_braces.splitlines():
+        if len(re.findall(r'\w+\-\w+\-?\w?', line)) == 1:
+            continue
+        if len(re.findall(r'\w+', line)) == 1 or not line.strip():
+            continue
+        if line_num == 1:
+            formated = line+'\n'
+            line_num+=1
+            continue
+        formated1 = re.sub("(^\s+)(?=\w)", r'\1"', line,)
+        formated2 = re.sub("(\w)(?=\s)", r'\1":', formated1, 1)
+        if line_num == (num_of_lines-1):
+            formated3 = re.sub("(?<=\s)(\w.*)", r'"\1"\n', formated2, 1)
+        else:
+            formated3 = re.sub("(?<=\s)(\w.*)", r'"\1",\n', formated2, 1)
+        line_num+=1
+        formated = formated+formated3
+    parsed_json = json.loads(formated)
+    return parsed_json
+
+
+
+if __name__ == "__main__":
     while True:
         redis_queue_push(get_requests())
 
@@ -99,9 +117,9 @@ if __name__ == "__main__":
             
                 try:
                     if req_cmd != "" and req_port_mode == "":
-                        output = str(run_command_on_device_wo_close(req_switch_ip, switch_user, switch_pass, req_cmd)).replace('\\n', '\n').replace('\\r', '\r').replace("'!","")
+                        output = json_parser(str(run_command_on_device_wo_close(req_switch_ip, switch_user, switch_pass, req_cmd)))
                     else:
-                        output = change_interface_mode(req_switch_ip, switch_user, switch_pass, req_interface_name, req_port_mode, req_vlans)
+                        output = str(change_interface_mode(req_switch_ip, switch_user, switch_pass, req_interface_name, req_port_mode, req_vlans))
                 except Exception as error:
                     send_status_update(req_id, "failed", error)
                 else:
@@ -112,3 +130,4 @@ if __name__ == "__main__":
                 continue
 
         time.sleep(10)
+            
